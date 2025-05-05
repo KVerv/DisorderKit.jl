d08393c41a9f98130eb7da77611b9909ccaef4c2
KVerv <kevin.vervoort@hotmail.com>
2025-05-05 14:07:58 +0200

    Added constant definitions

--- src/DisorderKit.jl
+++ src/DisorderKit.jl
@@ -6,5 +6,9 @@ using TensorKit, MPSKit, KrylovKit
 using LinearAlgebra
 using Crayons, TimerOutputs
 
+const AbstractMPSTensor = AbstractTensorMap{T, S, 2, 1} where {T, S}
+const AbstractMPOTensor = AbstractTensorMap{T, S, 2, 2} where {T, S}
+
+include("VOMPS_Inversion.jl")
 
 end # module DisorderKit
