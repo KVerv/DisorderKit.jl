80afee6383bfea997c05ceb49af6ef0232701bf5
KVerv <kevin.vervoort@hotmail.com>
2025-05-05 12:10:09 +0200

    added initialization of the VOMPS inversion code

--- src/VOMPS_Inversion.jl
+++ src/VOMPS_Inversion.jl
@@ -11,3 +11,43 @@ struct  VOMPS_Inversion <: AbstractInversionAlgorithm
         return new(inverse_dim, tol, maxiter, verbose, finalize)
     end
 end
+
+# Convention for MPO:
+#       3
+#       v
+#     1<+<4
+#       v
+#       2
+
+function transform_to_mps(Ts::InfiniteMPO)
+    mps = map(Ts) do T
+        iso = isomorphism(fuse(space(T)[2],space(T)[3]),space(T)[2]⊗space(T)[3])
+        @tensor T_mps[-1 -2; -3] := T[-1 1; 2 -4]*iso[-2; 1 2]
+        return T_mps
+    end
+    return InfiniteMPS(mps)
+end
+
+# Inversion of MPO through VOMPS Algorihtm
+function invert_mpo(Os::InfiniteMPO, alg::VOMPS_Inversion; init_guess::InfiniteMPO = nothing)
+    unit_cell = length(t)
+    # Make initial guess
+    if isnothing(init_guess) 
+        inits = [TensorMap(rand, ComplexF64, space(Os[i])) for i in 1:length(t)]
+        init_guess = InfiniteMPO(inits)
+    end
+    # Convert initial guess to MPSKit
+    As = transform_to_mps(init_guess)
+
+    # Bring MPS in canonical forms
+    ALs = As.AL
+    ARs = As.AR
+    ACs = As.AC
+    Cs  = As.C
+
+    i = 0
+    ϵ = 1
+    while ϵ > alg.tol && i < alg.maxiter
+
+    end
+end
