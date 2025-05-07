213127d65cfe41256ec54173e621ae9e6dc81367
KVerv <kevin.vervoort@hotmail.com>
2025-05-07 15:11:22 +0200

    Add tests for VOMPS_Inversion

---
+++ test/runtests.jl
@@ -0,0 +1,7 @@
+using Test
+using Revise
+using TensorKit, MPSKit
+using LinearAlgebra
+using DisorderKit
+
+include("test_VOMPS_Inversion.jl")
---
+++ test/test_VOMPS_Inversion.jl
@@ -0,0 +1,59 @@
+@testset "test invert_mpo with VOMPS: random trivial bond dimension MPO" for ix in 1:10
+    d = 2
+    alg = VOMPS_Inversion(1; tol=1e-8, maxiter=20, verbose=true)
+    for unit_cell = 1:5
+        Os = [TensorMap(rand, ComplexF64, ℂ^1 ⊗ ℂ^d, ℂ^d ⊗ ℂ^1) for i in 1:unit_cell]
+
+        mpo = InfiniteMPO(Os)
+        Oinv, ϵ = invert_mpo(mpo, alg; init_guess = nothing)
+
+        for i in 1:unit_cell
+            O_times_Oinv = reshape(Os[i].data,(2,2)) * reshape(Oinv[i].data, (2,2))
+            @test norm(O_times_Oinv - O_times_Oinv[1, 1] * Matrix{ComplexF64}(I, d, d)) < 1e-8
+        end
+    end
+end
+
+@testset "test invert_mpo with VOMPS: random trivial physical dimension MPO" for ix in 1:10
+    d = 1 #FIXME does not work for d > 1
+    alg = VOMPS_Inversion(1; tol=1e-8, maxiter=20, verbose=true)
+    for unit_cell = 1:5
+        Os = [TensorMap(rand, ComplexF64, ℂ^d ⊗ ℂ^1, ℂ^1 ⊗ ℂ^d) for i in 1:unit_cell]
+
+        mpo = InfiniteMPO(Os)
+        Oinv, ϵ = invert_mpo(mpo, alg; init_guess = nothing)
+
+        for i in 1:unit_cell
+            O_times_Oinv = mpo * Oinv
+            ϵ = test_identity(O_times_Oinv)
+            @test ϵ < 1e-8
+        end
+    end
+end
+
+
+@testset "test invert_mpo with VOMPS: exp(diagonal Hamiltonian) #1" for τ in (1:17) * 0.1
+    function expZZ(τ::Real)
+        σz = TensorMap(ComplexF64[1 0; 0 -1], ℂ^2, ℂ^2)
+        M = σz ⊗ σz
+        L, S, R = tsvd(exp(-τ * M), (1, 3), (2, 4), trunc=truncerr(1e-10))
+        L = permute(L * sqrt(S), (1, ), (2, 3))
+        R = permute(sqrt(S) * R, (1, 2), (3, ))
+        @tensor T1[-1 -2; -3 -4] := L[-2; 1 -4] * R[-1 1 ; -3]
+        @tensor T2[-1 -2; -3 -4] := R[-1 -2; 1] * L[1; -3 -4]
+        return InfiniteMPO([T1])
+    end
+
+    @show τ
+    mpo = expZZ(τ)
+    O_inv_exact = expZZ(-τ)
+
+    d = 2
+    alg = VOMPS_Inversion(d; tol=1e-8, maxiter=500, verbose=true)
+    
+    Oinv, ϵ = invert_mpo(mpo, alg; init_guess = nothing)
+
+    O_times_Oinv = mpo * Oinv
+    ϵ = test_identity(O_times_Oinv)
+    @test ϵ < 1e-8
+end
