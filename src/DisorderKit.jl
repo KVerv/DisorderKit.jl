0baa7e3a65da1c0a192d04818afe8e62084bbe3a
KVerv <kevin.vervoort@hotmail.com>
2025-05-07 15:11:38 +0200

    update DisorderKit.jl

--- src/DisorderKit.jl
+++ src/DisorderKit.jl
@@ -6,9 +6,16 @@ using TensorKit, MPSKit, KrylovKit
 using LinearAlgebra
 using Crayons, TimerOutputs
 
+export VOMPS_Inversion, invert_mpo 
+export test_identity
+
 const AbstractMPSTensor = AbstractTensorMap{T, S, 2, 1} where {T, S}
 const AbstractMPOTensor = AbstractTensorMap{T, S, 2, 2} where {T, S}
+const AbstractRhoEnv = AbstractTensor{T, S, 2} where {T, S}
+const AbstractEEnv = AbstractTensor{T, S, 4} where {T, S}
+const AbstractBondTensor = AbstractTensorMap{T, S, 1, 1} where {T, S}
 
 include("VOMPS_Inversion.jl")
+include("utils.jl")
 
 end # module DisorderKit
