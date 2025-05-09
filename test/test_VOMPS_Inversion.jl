ceb2f6dd3d6dc5d860144ae762d8102f7007c2d2
KVerv <kevin.vervoort@hotmail.com>
2025-05-09 11:58:28 +0200

    Add Trotter Gates Ising test

--- test/test_VOMPS_Inversion.jl
+++ test/test_VOMPS_Inversion.jl
@@ -60,3 +60,42 @@ end
     @show τ
     @test ϵ < 1e-8
 end
+
+
+@testset "test invert_mpo with VOMPS: trotter gates (Ising)" for τ in 0.1:0.1:0.7, t in 0:0.01:0.02
+    function gen_expZZX(τ::Real)
+        Id = TensorMap(ComplexF64[1 0; 0 1], ℂ^2, ℂ^2)
+        σz = TensorMap(ComplexF64[1 0; 0 -1], ℂ^2, ℂ^2)
+        σx = TensorMap(ComplexF64[0 1; 1 0], ℂ^2, ℂ^2)
+        M = σz ⊗ σz + 1/2*σx⊗Id +1/2*Id⊗σx
+        L, S, R = tsvd(exp(-τ * M), (1, 3), (2, 4), trunc=truncerr(1e-10))
+        L = permute(L * sqrt(S), (1, ), (2, 3))
+        R = permute(sqrt(S) * R, (1, 2), (3, ))
+        @tensor T1[-1 -2; -3 -4] := L[-2; 1 -4] * R[-1 1 ; -3]
+        @tensor T2[-1 -2; -3 -4] := R[-1 -2; 1] * L[1; -3 -4]
+        return InfiniteMPO([T1, T2])
+    end
+
+    @show τ, t
+    mpo = gen_expZZX(τ)
+    mpo_inv = gen_expZZX(-τ)
+    
+    # initial guess
+    A = TensorMap(randn, ComplexF64, space(mpo_inv[1]))
+    B = TensorMap(randn, ComplexF64, space(mpo_inv[2]))
+    mpo_inv[1] += t*A 
+    mpo_inv[2] += t*B 
+
+    maxiter = norm(t) < 1e-15 ? 1 : 200
+    inverse_dim = dim(space(mpo_inv[1])[1])
+    alg = VOMPS_Inversion(inverse_dim; tol=1e-8, maxiter=500, verbose=true)
+    
+    Oinv, ϵ = invert_mpo(mpo, alg; init_guess = mpo_inv)
+    @test ϵ < 1e-8
+    
+    O_times_Oinv = mpo * Oinv
+    mps = DisorderKit.transform_to_mps(mpo * Oinv)
+    ϵ_acc = test_identity(O_times_Oinv)
+    @show τ, t
+    @test ϵ_acc < 1e-8
+end
