860f350c39beadfd97e9273cc830e2ba31607ec3
KVerv <kevin.vervoort@hotmail.com>
2025-05-14 15:22:50 +0200

    add utitlity functions for truncation

--- src/utils.jl
+++ src/utils.jl
@@ -18,9 +18,11 @@ function entanglement_spectrum(Os::InfiniteMPO, i::Int)
     unit_cell = length(Os)
     transfer_l = transfer_left_mpo(Os[i+1])
     transfer_r = transfer_right_mpo(Os[i])
-    for j = 1:unit_cell-1
-        transfer_l = transfer_left_mpo(Os[i+1+j]) ∘ transfer_l
-        transfer_r = transfer_right_mpo(Os[i-j]) ∘ transfer_r
+    for j = i+2:i+unit_cell
+        transfer_l = transfer_left_mpo(Os[j]) ∘ transfer_l
+    end
+    for j = i-1:-1:i-unit_cell+1
+        transfer_r = transfer_right_mpo(Os[j]) ∘ transfer_r
     end
 
     Dl = space(Os[i+1], 1)
@@ -51,20 +53,42 @@ end
 
 # MPO environments used in truncation
 function env_left(Os::InfiniteMPO, ix::Int)
-    v1 = TensorMap(rand, ComplexF64, space(Os[ix + 1], 1), space(Os[ix + 1], 1))
-    transfer_left = transfer_leftt_mpo(Os[ix])
-    for jx in ix+1:1:ix+length(Os)-1
-        transfer_left = transfer_leftt_mpo(Os[jx]) ∘ transfer_left
+    v1 = TensorMap(rand, ComplexF64, space(Os[ix+1], 1), space(Os[ix+1], 1))
+    transfer_left = transfer_left_mpo(Os[ix+1])
+    for jx in ix+2:1:ix+length(Os)
+        transfer_left = transfer_left_mpo(Os[jx]) ∘ transfer_left
     end
-    _, ls = eigsolve(v -> transfer_left(Os, ix, v), v1, 1, :LM);
+    _, ls = eigsolve(v -> transfer_left(v), v1, 1, :LM);
     return ls[1]
 end
+
 function env_right(Os::InfiniteMPO, ix::Int)
     v1 = TensorMap(rand, ComplexF64, space(Os[ix + 1], 1), space(Os[ix + 1], 1))
     transfer_right = transfer_right_mpo(Os[ix])
     for jx in ix-1:-1:ix-length(Os)+1
         transfer_right = transfer_right_mpo(Os[jx]) ∘ transfer_right
     end
-    _, ls = eigsolve(v -> transfer_right(Os, ix, v), v1, 1, :LM);
+    _, ls = eigsolve(v -> transfer_right(v), v1, 1, :LM);
     return ls[1]
+end
+
+function mpo_ovlp(A1::InfiniteMPO, A2::InfiniteMPO)
+    V1 = space(A1[1], 1)
+    V2 = space(A2[1], 1)
+
+    function mpo_transf(v)
+        for (M1, M2) in zip(A1, A2)
+            @tensor Tv[-1; -2] := M1[1 3; 4 -2] * conj(M2[2 3; 4 -1]) * v[2; 1]
+            v = Tv
+        end
+        return v
+    end
+
+    v0 = TensorMap(rand, ComplexF64, V2, V1)
+    λs, _ = eigsolve(mpo_transf, v0, 1, :LM)
+    return λs[1]
+end
+
+function mpo_fidelity(A1::InfiniteMPO, A2::InfiniteMPO)
+    return norm(mpo_ovlp(A1, A2) / sqrt(mpo_ovlp(A1, A1) * mpo_ovlp(A2, A2)))
 end
