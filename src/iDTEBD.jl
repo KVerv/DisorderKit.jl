31d137e82be6f4d3597cc0d758fc373b8f0bec50
KVerv <kevin.vervoort@hotmail.com>
2025-05-14 15:23:28 +0200

    minor fix

--- src/iDTEBD.jl
+++ src/iDTEBD.jl
@@ -12,32 +12,32 @@ struct  iDTEBD <: AbstractAlgorithm
     timer_output::TimerOutput
 
     function iDTEBD(alg_inversion::AbstractInversionAlgorithm, alg_trunc_Z::AbstractTruncationAlgorithm, alg_trunc_disordermpo::AbstractTruncationAlgorithm; invtol::Float64 = 1e-8, nsteps::Int = 50, verbosity::Int = 0, truncfrequency::Int = 1,  timer_output::TimerOutput = TimerOutput())
-        return new(alg_inversion, alg_trunc_Z, alg_trunc_disordermpo; invtol, nsteps, verbosity, truncfrequency, timer_output)
+        return new(alg_inversion, alg_trunc_Z, alg_trunc_disordermpo, invtol, nsteps, verbosity, truncfrequency, timer_output)
     end
 end
 
 function evolve_densitymatrix(Ts::DisorderMPO, ps::Vector{<:Real}, alg::iDTEBD; ρ0 = nothing)
     ρs = isnothing(ρ0) ? deepcopy(Ts) : ρ0
-    χ = alg.alg_inversion.inverse_dim
     mpoZinv = nothing
+    alg_inversion = alg.alg_inversion
     for ix in 1:alg.nsteps
         (alg.verbosity > 0) && (@info "Iteration $ix)")
         (alg.verbosity > 1) && (@info(crayon"magenta"("Before normalization: Bonddimension of ρ = $(dim(space(ρs[1])[1]))")))
-        (alg.verbosity > 1) && (@info(crayon"magenta"("Using Z⁻¹ bonddimension of χ = $(χ)")))
+        (alg.verbosity > 1) && (@info(crayon"magenta"("Using Z⁻¹ bonddimension of χ = $(alg_inversion.inverse_dim)")))
         @timeit alg.timer_output "normalize_each_disorder_sector" begin
-            ρ_normalized, ϵ_acc, mpoZinv = normalize_each_disorder_sector(ρs, alg.alg_trunc_Z, alg.alg_inversion; init_guess = mpoZinv, verbosity = alg.verbosity)
+            ρs_normalized, ϵ_acc, mpoZinv = normalize_each_disorder_sector(ρs, alg.alg_trunc_Z, alg_inversion; init_guess = mpoZinv, verbosity = alg.verbosity, invtol = alg.invtol)
         end
         while ϵ_acc > alg.invtol
-            χ *= 2
-            (alg.verbosity > 1) && (@info(crayon"magenta"("Using Z⁻¹ bonddimension of χ = $(χ)")))
+            (alg.verbosity > 1) && (@info(crayon"magenta"("Using Z⁻¹ bonddimension of χ = $(alg_inversion.inverse_dim)")))
+            alg_inversion = VOMPS_Inversion(alg_inversion.inverse_dim*2; tol = alg_inversion.tol, maxiter = alg_inversion.maxiter, verbosity = alg_inversion.verbosity)
             @timeit alg.timer_output "normalize_each_disorder_sector" begin
-                ρ_normalized, ϵ_acc, mpoZinv = normalize_each_disorder_sector(ρs, alg.alg_trunc_Z, alg.alg_inversion; init_guess = mpoZinv, verbosity = alg.verbosity)
+                ρs_normalized, ϵ_acc, mpoZinv = normalize_each_disorder_sector(ρs, alg.alg_trunc_Z, alg_inversion; init_guess = nothing, verbosity = alg.verbosity,  invtol = alg.invtol)
             end
         end
         if mod(ix, alg.truncfrequency) == 0
             (alg.verbosity > 0) && (@info(crayon"magenta"("Truncating ρ")))
             (alg.verbosity > 1) && (@info(crayon"magenta"("Before truncation: Bonddimension of ρ = $(dim(space(ρs_normalized[1])[1]))")))
-            @timeit alg.timer_output "truncate_disorder_MPO" ρs = truncate_disorder_MPO(ρ_normalized, ps, alg.alg_trunc_disordermpo)
+            @timeit alg.timer_output "truncate_disorder_MPO" ρs = truncate_mpo(ρs_normalized, ps, alg.alg_trunc_disordermpo)
             (alg.verbosity > 1) && (@info(crayon"magenta"("After truncation: Bonddimension of ρ = $(dim(space(ρs[1])[1]))")))
         end
         (alg.verbosity > 0) && (@info(crayon"magenta"("Evolve")))
