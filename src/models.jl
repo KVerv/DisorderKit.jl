ec6da6395608876d35b5d0e5e8eb03cf2f8a50c2
KVerv <kevin.vervoort@hotmail.com>
2025-06-03 16:37:06 +0200

    models tweaking

--- src/models.jl
+++ src/models.jl
@@ -4,21 +4,32 @@ function RTFIM_hamiltonian(Js::Vector{Float64}, hs::Vector{Float64})
     Z[1, 1], Z[2, 2] = 1, -1
     Id[1, 1], Id[2, 2] = 1, 1
     D_disorder = length(Js) * length(hs)
-    Hs = zeros(ComplexF64, 3, 2, D_disorder, 2, D_disorder, 3)
+    # Hs = zeros(ComplexF64, 3, 2, D_disorder, 2, D_disorder, 3)
+    Hs = zeros(ComplexF64,ℂ^3⊗ℂ^2⊗ℂ^D_disorder,ℂ^2⊗ℂ^D_disorder⊗ℂ^3)
     for (i, (h, J)) in enumerate(Iterators.product(hs, Js))
-        @show i, h, J
-        Hs[1,:,i, :, i, 1] = Id
-        Hs[2,:,i, :, i, 1] = zeros(ComplexF64, 2, 2)
-        Hs[3,:,i, :, i, 1] = zeros(ComplexF64, 2, 2)
-        Hs[1,:,i, :, i, 2] = -J*Z
-        Hs[2,:,i, :, i, 2] = zeros(ComplexF64, 2, 2)
-        Hs[3,:,i, :, i, 2] = zeros(ComplexF64, 2, 2)
-        Hs[1,:,i, :, i, 3] = -h*X
-        Hs[2,:,i, :, i, 3] = Z
-        Hs[3,:,i, :, i, 3] = Id
+        H = transverse_field_ising(; J = J, g = h/J)
+        U = convert(TensorMap,H[1])
+        disordermap = DiagonalTensorMap(zeros(ComplexF64, D_disorder),ℂ^D_disorder)
+        disordermap[i,i] = 1.0
+        @tensor U_full[-1 -2 -3; -4 -5 -6] := U[-1 -2; -4 -6]*disordermap[-3; -5]
+        if i == 1
+            Hs = U_full
+        else
+            Hs += U_full
+        end
+        # @show i, h, J
+        # Hs[1,:,i, :, i, 1] = Id
+        # Hs[2,:,i, :, i, 1] = zeros(ComplexF64, 2, 2)
+        # Hs[3,:,i, :, i, 1] = zeros(ComplexF64, 2, 2)
+        # Hs[1,:,i, :, i, 2] = -J*Z
+        # Hs[2,:,i, :, i, 2] = zeros(ComplexF64, 2, 2)
+        # Hs[3,:,i, :, i, 2] = zeros(ComplexF64, 2, 2)
+        # Hs[1,:,i, :, i, 3] = -h*X
+        # Hs[2,:,i, :, i, 3] = Z
+        # Hs[3,:,i, :, i, 3] = Id
     end 
 
-    Hs = TensorMap(Hs, ℂ^3⊗ℂ^2⊗ℂ^D_disorder,ℂ^2⊗ℂ^D_disorder⊗ℂ^3)
+    # Hs = TensorMap(Hs, ℂ^3⊗ℂ^2⊗ℂ^D_disorder,ℂ^2⊗ℂ^D_disorder⊗ℂ^3)
 
     return DisorderMPO([Hs])
 end
@@ -44,13 +55,14 @@ function random_transverse_field_ising_evolution(Js::Vector{Float64}, hs::Vector
 
     # expHs2 = TensorMap(expHs, ℂ^2⊗ℂ^2⊗ℂ^D_disorder,ℂ^2⊗ℂ^D_disorder⊗ℂ^2)
 
-    alg = TaylorCluster(; N=order, extension=true, compression=true)
+    alg = TaylorCluster(; N=order, extension=false, compression=false)
+    # alg = DisorderKit.ClusterExpansion(order)
     D_disorder = length(Js) * length(hs)
     expHs = 0
     for (i, (h, J)) in enumerate(Iterators.product(hs, Js))
         @show i, h, J
         H = transverse_field_ising(; J = J, g = h/J)
-        expH = make_time_mpo(H, -1im*dβ, alg)
+        expH = MPSKit.make_time_mpo(H, -1im*dβ, alg)
         U = convert(TensorMap,expH[1])
         disordermap = DiagonalTensorMap(zeros(ComplexF64, D_disorder),ℂ^D_disorder)
         disordermap[i,i] = 1.0
