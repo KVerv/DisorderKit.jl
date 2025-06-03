da61266d44d96a43c8c0dafae1290d985c48d953
KVerv <kevin.vervoort@hotmail.com>
2025-06-03 16:35:22 +0200

    Minor

--- src/DisorderKit.jl
+++ src/DisorderKit.jl
@@ -2,15 +2,15 @@ module DisorderKit
 
 __precompile__(true)
 
-using TensorKit, MPSKit, MPSKitModels, KrylovKit
+using TensorKit, MPSKit, MPSKitModels, KrylovKit, BlockTensorKit
 using LinearAlgebra
 using Crayons, TimerOutputs
 
 export VOMPS_Inversion, invert_mpo 
 export test_identity, mpo_fidelity
-export DisorderMPO, measure, partition_functions, disorder_average, average_correlation_length
+export DisorderMPO, measure, partition_functions, disorder_average, average_correlation_length, normalize_each_disorder_sector
 export StandardTruncation, DisorderTracedTruncation, truncate_mpo
-export iDTEBD, evolve_densitymatrix
+export iDTEBD, evolve_densitymatrix, evolve_one_time_step
 export random_transverse_field_ising_evolution, RTFIM_time_evolution_Trotter, RBH_time_evolution_Trotter, RTFIM_hamiltonian
 
 const AbstractMPSTensor = AbstractTensorMap{T, S, 2, 1} where {T, S}
@@ -27,5 +27,6 @@ include("DisorderMPO.jl")
 include("mpo_truncation.jl")
 include("iDTEBD.jl")
 include("models.jl")
+include("cluster_expansion.jl")
 
 end # module DisorderKit
