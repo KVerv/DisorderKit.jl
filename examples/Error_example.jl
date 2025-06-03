f1a67704e70d9afc0cbb9423e120728b7940f86c
KVerv <kevin.vervoort@hotmail.com>
2025-06-03 16:37:39 +0200

    Tweak examples

--- examples/Error_example.jl
+++ examples/Error_example.jl
@@ -1,35 +1,43 @@
 using Revise, TensorKit, MPSKit, MPSKitModels, DisorderKit, TimerOutputs, CairoMakie
 
 # Define model
-N = 3 
-a = 0.7
-b = 1.33
+N = 4
+a = 0.8
+b = 1.2
 
-Js = [1.0]
-hs = Vector(a:(b-a)/(N-1):b)
-# hs = [1.0]
-ps = ones(N)./N
-dτ = 2e-2
+# Js = [1.0]
+# hs = Vector(a:(b-a)/(N-1):b)
+# # hs = [1.0]
+# ps = ones(N)./N
+dτ = 5e-2
+
 
 # U = disorder_average(Us, [1.])
 
 # Us = RTFIM_time_evolution_Trotter(dτ, hs, Js)
 # Us = DisorderMPO([Us[1]])
-Hs = RTFIM_hamiltonian(Js, hs)
+# Hs = RTFIM_hamiltonian(Js, hs)
 
 # Define algorithms
-invtol = 1e-1
-trunctol = 1e-8
-D_max = 25
+invtol = 1e-6
+trunctol = 1e-6
+D_max = 50
 alg_inversion = VOMPS_Inversion(1; tol = 1e-8, maxiter = 250, verbosity = 2)
 alg_trunc_Z = StandardTruncation(trunc_method = truncerr(trunctol))
 alg_trunc_disordermpo = DisorderTracedTruncation(trunc_method = truncdim(D_max))
 
-βs = 0.1:0.1:10
+βs = 1:0.5:20
 # Evolve density matrix
-function get_ξ(βs, Us, m)
+function fixed_point_distribution_h(h,β)
+    Γ = log(β)
+    y = h/Γ
+    return  1/Γ*exp(-y)
+end
+
+function get_ξ(βs, Us, ps)
     ξs = zeros(length(βs))
     Es = zeros(ComplexF64,length(βs))
+    DZs = zeros(Int,length(βs))
     ϵs = []
     ρ0 = nothing
     nsteps = round(Int, βs[1]/dτ)
@@ -39,39 +47,90 @@ function get_ξ(βs, Us, m)
             dβ = βs[i] - βs[i-1]
             nsteps = round(Int, dβ/dτ)
         end
-        alg_evolution = iDTEBD(alg_inversion, alg_trunc_Z, alg_trunc_disordermpo; invtol = invtol, nsteps = nsteps, verbosity = 2, truncfrequency = 1, inversion_frequency = m, timer_output = TimerOutput())
+        inversion_frequency = 1
+        alg_evolution = iDTEBD(alg_inversion, alg_trunc_Z, alg_trunc_disordermpo; invtol = invtol, nsteps = nsteps, verbosity = 2, truncfrequency = 1, inversion_frequency = inversion_frequency, timer_output = TimerOutput(), max_inverse_dim = 2)
         ρs, ϵ = evolve_densitymatrix(Us, ps, alg_evolution; ρ0 = ρ0)
         ξs[i] = average_correlation_length(ρs, ps)
-        Es[i] = (measure(ρs, ps, Hs, 1))
+        # Es[i] = (measure(ρs, ps, Hs, 1))
+        Es[i] = 1
+        Z = partition_functions(ρs)
+        Z = truncate_mpo(Z, alg_trunc_Z)
+        @show DisorderKit.test_identity_random(Z)
+        DZs[i] = dim(space(Z[1])[1])
         ρ0 = ρs
         push!(ϵs, ϵ...)
     end
-    return ξs, Es, ϵs
+    return ξs, Es, ϵs, DZs
 end
-
-fig2 = Figure(backgroundcolor=:white, fontsize=30, size=(1000, 1000))
-ax3 = Axis(fig2[1, 1], 
-        xlabel = L"$β$",
-        ylabel = L"$ϵ$",
-        # xscale = log10,
-        yscale = log10
-        )
-
-for m in 10:10
-    n = 2
+set_theme!(theme_latexfonts())
+    fig = Figure(backgroundcolor=:white, fontsize=30, size=(1000, 1000))
+    ax1 = Axis(fig[1, 1], 
+            xlabel = L"$β$",
+            ylabel = L"$ξ$",
+            # xscale = log10,
+            # yscale = log10
+            )
+    # ax2 = Axis(fig[1, 2], 
+    #         xlabel = L"$(\ln{β})$",
+    #         ylabel = L"$C_v$",
+    #         # xscale = log10,
+    #         # yscale = log10
+    #         )
+    ax2 = Axis(fig[1, 2], 
+            xlabel = L"$\ln β$",
+            ylabel = L"$ϵ$",
+    #         # xscale = log10,
+            yscale = log10
+            )
+    ax3 = Axis(fig[2, 1], 
+    xlabel = L"$(\ln β)^2$",
+    ylabel = L"$\frac{d\xi}{d\ln{\beta}^2}$",
+#         # xscale = log10,
+    # yscale = log10
+    )
+    ax4 = Axis(fig[2, 2], 
+    xlabel = L"$β$",
+    ylabel = L"$D_\mathcal{Z}$",
+#         # xscale = log10,
+    # yscale = log10
+    )
+for n in [0, 2]
     if n == 0
+        # Us = RTFIM_time_evolution_Trotter(dτ, hs, Js)
+        Us = RTFIM_time_evolution_Trotter(dτ, [1.0], [1.0])
+        Us = DisorderMPO([Us[1]])
+        ps = [1.]
+    else
+        Js = [1.0]
+        hs = Vector(a:(b-a)/(N-1):b)
+        # hs = [1.0]
+        ps = ones(N)./N
+
+        # hs = [0.9, 1.1]
+        # ps = [0.5, 0.5]
+        ps = ps ./ sum(ps)
+        @show ps
+        # Us = random_transverse_field_ising_evolution(Js, hs, dτ; order=n)
+        # Us = RTFIM_time_evolution_Trotter(dτ, [1.], [1.1])
         Us = RTFIM_time_evolution_Trotter(dτ, hs, Js)
         Us = DisorderMPO([Us[1]])
-    else
-        Us = random_transverse_field_ising_evolution(Js, hs, dτ; order=n)
+        # ps = [1.]
     end
-    ξs, Es, ϵs = get_ξ(βs, Us, m)
+    ξs, Es, ϵs, DZs = get_ξ(βs, Us, ps)
 
     Cvs = real.(-βs[1:end-1].^2 .*diff(Es)./diff(βs))
 
+    # dxidlnβ2 = diff(ξs)./diff(log.(βs).^2)
+    dxidlnβ2 = diff(ξs)./diff(βs)
+    # Plot correlation lengths in function of β
+    # scatter!(ax1,log.(βs).^2,ξs, label=L"$n=%$n$, $Δτ=%$dτ$",markersize = 16)
+    scatter!(ax1,βs,ξs, label=L"$n=%$n$, $Δτ=%$dτ$",markersize = 16)
+    # scatter!(ax2,log.(βs[1:end-1]),Cvs[1:end], label=L"$D=%$D_max$, $Δτ=%$dτ$",markersize = 16)
+    scatter!(ax2,log.(dτ:dτ:βs[end]).^2,ϵs.+1e-16, label=L"$n=%$n$, $Δτ=%$dτ$",markersize = 16)
+    scatter!(ax3,log.(βs[1:end-1]).^2, dxidlnβ2, label=L"$n=%$n$, $Δτ=%$dτ$",markersize = 16)
+    scatter!(ax4,log.(βs).^2,DZs, label=L"$n=%$n$, $Δτ=%$dτ$",markersize = 16)
 
-    scatter!(ax3,dτ:dτ:βs[end],ϵs.+1e-16, label=L"$n = %$m$ $Δτ=%$dτ$",markersize = 16)
 end
-lines!(ax3,dτ:dτ:βs[end],ones(length(dτ:dτ:βs[end]))*invtol)
-axislegend(ax3, position=:lt)
-fig2
+axislegend(ax1, position=:lt)
+# axislegend(ax2, position=:lt)
+fig
--- examples/RTFIM_example.jl
+++ examples/RTFIM_example.jl
@@ -1,17 +1,17 @@
-using Revise, TensorKit, MPSKit, MPSKitModels, DisorderKit, TimerOutputs, CairoMakie
+using Revise, TensorKit, MPSKit, MPSKitModels, DisorderKit, TimerOutputs, CairoMakie, LsqFit
 
 # Define model
-N = 5 
+N = 3
 a = 0.7
-b = 1.33
+b = 1.3
 
 Js = [1.0]
-hs = Vector(a:(b-a)/(N-1):b)
-# hs = [1.0]
-ps = ones(N)./N
-dτ = 1e-1
+# hs = Vector(a:(b-a)/(N-1):b)
+# ps = ones(N)./N
+hs = [0.99]
+ps = [1.0]
+dτ = 5e-2
 
-Us = random_transverse_field_ising_evolution(Js, hs, dτ; order=2)
 # U = disorder_average(Us, [1.])
 
 # Us = RTFIM_time_evolution_Trotter(dτ, hs, Js)
@@ -19,18 +19,19 @@ Us = random_transverse_field_ising_evolution(Js, hs, dτ; order=2)
 Hs = RTFIM_hamiltonian(Js, hs)
 
 # Define algorithms
-invtol = 5e-1
-trunctol = 1e-8
-D_max = 20
+invtol = 1e-6
+trunctol = 1e-6
+D_max = 30
 alg_inversion = VOMPS_Inversion(1; tol = 1e-8, maxiter = 250, verbosity = 2)
 alg_trunc_Z = StandardTruncation(trunc_method = truncerr(trunctol))
 alg_trunc_disordermpo = DisorderTracedTruncation(trunc_method = truncdim(D_max))
 
-βs = 1:0.4:4
+βs = 1:1:25
 # Evolve density matrix
 function get_ξ(βs, Us)
     ξs = zeros(length(βs))
     Es = zeros(ComplexF64,length(βs))
+    DZs = zeros(Int,length(βs))
     ϵs = []
     ρ0 = nothing
     nsteps = round(Int, βs[1]/dτ)
@@ -41,36 +42,83 @@ function get_ξ(βs, Us)
             nsteps = round(Int, dβ/dτ)
         end
         inversion_frequency = 1
-        alg_evolution = iDTEBD(alg_inversion, alg_trunc_Z, alg_trunc_disordermpo; invtol = invtol, nsteps = nsteps, verbosity = 2, truncfrequency = 1, inversion_frequency = inversion_frequency, timer_output = TimerOutput())
+        alg_evolution = iDTEBD(alg_inversion, alg_trunc_Z, alg_trunc_disordermpo; invtol = invtol, nsteps = nsteps, verbosity = 2, truncfrequency = 1, inversion_frequency = inversion_frequency, timer_output = TimerOutput(), max_inverse_dim = 4)
         ρs, ϵ = evolve_densitymatrix(Us, ps, alg_evolution; ρ0 = ρ0)
         ξs[i] = average_correlation_length(ρs, ps)
         Es[i] = (measure(ρs, ps, Hs, 1))
+        Z = partition_functions(ρs)
+        Z = truncate_mpo(Z, alg_trunc_Z)
+        @show DisorderKit.test_identity_random(Z)
         ρ0 = ρs
         push!(ϵs, ϵ...)
     end
-    return ξs, Es, ϵs
+    return ξs, Es, ϵs, DZs
 end
-
-ξs, Es, ϵs = get_ξ(βs, Us)
-
-Cvs = real.(-βs[1:end-1].^2 .*diff(Es)./diff(βs))
-
-# Plot correlation lengths in function of β
 set_theme!(theme_latexfonts())
-fig = Figure(backgroundcolor=:white, fontsize=30, size=(1000, 1000))
-ax1 = Axis(fig[1, 1], 
-        xlabel = L"$(\ln{β})^2$",
-        ylabel = L"$ξ$",
-        # xscale = log10,
-        # yscale = log10
-        )
-ax2 = Axis(fig[1, 2], 
-        xlabel = L"$(\ln{β})$",
-        ylabel = L"$C_v$",
-        # xscale = log10,
-        # yscale = log10
-        )
-scatter!(ax1,log.(βs).^2,ξs, label=L"$D=%$D_max$, $Δτ=%$dτ$",markersize = 16)
-# scatter!(ax1,log.(βs),ξs, label=L"$D=%$D_max$, $Δτ=%$dτ$",markersize = 16)
-scatter!(ax2,log.(βs[1:end-1]),Cvs[1:end], label=L"$D=%$D_max$, $Δτ=%$dτ$",markersize = 16)
+    fig = Figure(backgroundcolor=:white, fontsize=30, size=(1000, 1000))
+    ax1 = Axis(fig[1, 1], 
+            xlabel = L"$(\ln{β})^2$",
+            ylabel = L"$ξ$",
+            # xscale = log10,
+            # yscale = log10
+            )
+    # ax2 = Axis(fig[1, 2], 
+    #         xlabel = L"$(\ln{β})$",
+    #         ylabel = L"$C_v$",
+    #         # xscale = log10,
+    #         # yscale = log10
+    #         )
+    ax2 = Axis(fig[1, 2], 
+            xlabel = L"$\ln β$",
+            ylabel = L"$\langle E\rangle$",
+    #         # xscale = log10,
+            # yscale = log10
+            )
+    ax3 = Axis(fig[2, 1], 
+    xlabel = L"$(\ln β)^2$",
+    ylabel = L"$\frac{d\xi}{d\ln{\beta}^2}$",
+#         # xscale = log10,
+    # yscale = log10
+    )
+    ax4 = Axis(fig[2, 2], 
+    xlabel = L"$β$",
+    ylabel = L"$C_v$",
+#         # xscale = log10,
+    # yscale = log10
+    )
+for n in [0]
+    if n == 0
+        Us = RTFIM_time_evolution_Trotter(dτ, hs, Js)
+        Us = DisorderMPO([Us[1]])
+    else
+        Us = random_transverse_field_ising_evolution(Js, hs, dτ; order=n)
+    end
+    ξs, Es, ϵs, DZs = get_ξ(βs, Us)
+
+    Cvs = real.(-βs[1:end-1].^2 .*diff(Es)./diff(βs))
+
+    dxidlnβ2 = diff(log.(ξs))./diff(log.(βs))
+    # Plot correlation lengths in function of β
+    # scatter!(ax1,log.(βs),ξs, label=L"$n=%$n$, $Δτ=%$dτ$",markersize = 16)
+    # scatter!(ax1,βs,ξs, label=L"$n=%$n$, $Δτ=%$dτ$",markersize = 16)
+    scatter!(ax1,log.(βs),log.(ξs), label=L"$n=%$n$, $Δτ=%$dτ$",markersize = 16)
+    # scatter!(ax1,log.(βs),ξs, label=L"$D=%$D_max$, $Δτ=%$dτ$",markersize = 16)
+    # scatter!(ax2,log.(βs[1:end-1]),Cvs[1:end], label=L"$D=%$D_max$, $Δτ=%$dτ$",markersize = 16)
+    scatter!(ax2,βs,real.(Es), label=L"$n=%$n$, $Δτ=%$dτ$",markersize = 16)
+    scatter!(ax3,log.(βs[1:end-1]), dxidlnβ2, label=L"$n=%$n$, $Δτ=%$dτ$",markersize = 16)
+    scatter!(ax4,βs[1:end-1],Cvs, label=L"$n=%$n$, $Δτ=%$dτ$",markersize = 16)
+
+    xs = log.(βs)
+    ys = log.(ξs)
+    fitmax = 6
+    fitmin = 1
+    p0q = [1.,1.]
+    quadmodel(t,p) = p[1].+p[2]*t
+    quadfit = curve_fit(quadmodel, xs[fitmin:fitmax], ys[fitmin:fitmax], p0q)
+    b, a = quadfit.param
+    @show a, b
+    lines!(ax1,xs,a*xs.+b,color =:black)
+end
+axislegend(ax1, position=:lt)
+axislegend(ax2, position=:lt)
 fig
