abaf116535f00f202bc56c28538fac6a7b998642
KVerv <kevin.vervoort@hotmail.com>
2026-02-04 10:49:58 +0100

    Majorana ED example

--- examples/fermion_example.jl
+++ examples/fermion_example.jl
@@ -31,10 +31,24 @@ function skew_canonical(A::Matrix{<:Real})
     return Λ, U, Σ
 end
 
+function covariance_matrix(M::Matrix{<:Real}, β::Real)
+    _, U, Σ = skew_canonical(M)
+    N = size(M)[1] ÷ 2
+    ReC̃ = zeros(Float64, size(M))
+    for ix in 1:N
+        ReC̃[2*ix-1, 2*ix] = tanh(β*Σ[ix]/2)
+        ReC̃[2*ix, 2*ix-1] = -tanh(β*Σ[ix]/2)
+    end
+    ReC = U * ReC̃ * U' 
+    return ReC
+end
+
+
 function majorana_ED(L::Int, Js::Vector{Float64}, hs::Vector{Float64}; n_samples::Int=10)
     Jconfigs = all_combinations(Js, L-1)
     hconfigs = all_combinations(hs, L)
     GEs = []
+    Ms = []
     if n_samples < length(Jconfigs)*length(hconfigs)
         sampled_indices = rand(1:length(Jconfigs)*length(hconfigs), n_samples)
         sampled_Jh_configs = []
@@ -57,10 +71,18 @@ function majorana_ED(L::Int, Js::Vector{Float64}, hs::Vector{Float64}; n_samples
             H[2*n,2*n-1] = 2 * hconf[n] 
         end
         Λ, U, Σ = skew_canonical(reshape(H.data,(2L,2L)))
-        push!(GEs, -sum(filter(x -> x>=0, Σ.*0.5)))
+        # push!(GEs, -sum(filter(x -> x>=0, Σ.*0.5)))
+        push!(GEs, -sum(Σ.* 0.5))
+
+        # j = round(Int, L/2)
+        # C = covariance_matrix(reshape(H.data,(2L,2L)), 30.0)
+        # M = C[2j-1, 2j]
+        M = 0
+        push!(Ms, M)
+        # @show length(Σ)
     end
 
-    return sum(GEs)/length(GEs)
+    return sum(GEs)/length(GEs), sum(Ms)/length(Ms)
 end
 
 
@@ -70,21 +92,36 @@ end
 
 
 N = 2
-a = 0.7
-b = 1.3
-hs = Vector(a:(b-a)/(N-1):b)
-Js = hs
-# hs = [a]
+a = 0.5
+b = 1.0
+w = 0.0
+# hs = Vector(a:(b-a)/(N-1):b)
+# Js = hs
+# Js = [0.1]
+hs = [a, b]
+V = var(log.(hs);corrected = false)
+Js = [a, b]
+hs = [a, b]*exp(w*V)
+# Js = [1.0]
+# a = 0.7
+# b = 1.3
+# hs = Vector(a:(b-a)/(N-1):b)
 # Js = [b]
-Ls = [4, 6, 8, 10, 12, 14, 16, 18, 20]
+# hs = [a]
+Ls = [4, 6, 8, 10, 12]
+# Ls = [3, 5, 7, 9, 11, 13, 15]
+# Ls = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+# Ls = [6]
 
-FFs = majorana_ED.(Ls, Ref(Js), Ref(hs); n_samples=100000)
+result = majorana_ED.(Ls, Ref(Js), Ref(hs); n_samples=1000000)
+FFs = first.(result)
+Ms = last.(result)
 # tF = 1 .- Es[1]./FFs
 
 set_theme!(theme_latexfonts())
 fig = Figure(backgroundcolor=:white, fontsize=40, size=(1000, 600))
 ax1 = Axis(fig[1, 1], 
-        xlabel = L"L",
+        xlabel = L"1/L",
         ylabel = L"$E/L$",
         # xscale = log10,
         # yscale = log10
@@ -96,17 +133,26 @@ ax1 = Axis(fig[1, 1],
 # # yscale = log10
 # )
 
-scatter!(ax1,1 ./Ls,FFs./Ls, label=L"$FF$",markersize = 20)
+a = 1
+scatter!(ax1, 1 ./ (Ls).^(a), FFs./Ls, label=L"$FF$", markersize=20)
+maxfit = length(Ls)
+p0q = [1., 1.]
+linmodel(t, p) = p[1] .+ p[2] * t
+linfit = curve_fit(linmodel, 1 ./ (Ls).^(a), FFs./Ls, p0q)
+xs = [0, 1 ./ ((Ls).^(a))...]
+lines!(ax1, xs, linmodel(xs, linfit.param), color=:black, linewidth=2)
+fig
+
+Edens= FFs[end]/(Ls[end])
+
+EF = linfit.param[1]
+# t = E/EF
 # scatter!(ax1,Ls,Es, label=L"$MPS$",markersize = 20)
 # scatter!(ax2,1 ./Ls,abs.((FFs.-Es)./FFs), label=L"$data$",markersize = 20)
 # fig
-# J,h ∈[0.7, 1.3], D=40, conv = 5e-4
-# L | (FF-Emps)/FF
-# 3 | 10.93%
-# 4 | 9.36%
-# 5 | 8.33%
-# 6 | 7.06%
-# 7 | 6.75%
-# 8 | 6.49%
-# 9 | 5.73%
-# 10 | 5.41%
+
+# D | EF | EMPS D = 8 | EMPS/EF
+# -- | ------- | ---------- | -------
+# 0.2 | -1.27462 | -1.27493 | 100.03%
+# 0.4 | -1.28481 | -1.27955 | 99.60%
+# 0.6 | -1.28896 | -1.28305 | 99.04%| 99.23%| 99.23%
